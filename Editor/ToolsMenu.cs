026fb15aca766c22364a52a5736971b99d121ed0
Sergej <sgorey@bk.ru>
2022-08-03 17:16:46 +0800

    Init

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEngine.Application;
+using static UnityEditor.AssetDatabase;
+
+namespace TeaGames.Utilities.Editor
+{
+    public static class ToolsMenu
+    {
+        [MenuItem("Tools/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+            CreateDirectories("_Project", "Scripts", "Art", "Scenes");
+            Refresh();
+        }
+
+        public static void CreateDirectories(string root, params string[] dir)
+        {
+            var fullpath = Combine(dataPath, root);
+            foreach (var newDirectory in dir)
+            {
+                CreateDirectory(Combine(fullpath, newDirectory));
+            }
+        }
+    }
+}
---
+++ Runtime/MonoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+namespace TeaGames.Utilities.Runtime
+{
+    static class MonoExtensions
+    {
+        public static Action Delay(this MonoBehaviour mono, float delayInSeconds, Action action)
+        {
+            IEnumerator DelayCoroutine()
+            {
+                yield return new WaitForSeconds(delayInSeconds);
+                action?.Invoke();
+            }
+
+            mono.StartCoroutine(DelayCoroutine());
+            return action;
+        }
+
+        public static T RequireComponent<T>(this MonoBehaviour mono) where T : Component
+        {
+            if (mono.TryGetComponent<T>(out var component))
+                return component;
+            else
+                throw new MissingComponentException($"Missing {typeof(T).FullName} component of {mono.gameObject.name} game object!");
+        }
+    }
+}
