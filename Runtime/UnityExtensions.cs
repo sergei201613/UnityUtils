104c6c46666d9a790afa0bfd2a58ba33b282e165
sergei201613 <sgorey@bk.ru>
2022-09-04 16:24:06 +0800

    Edit UnityExtensions.cs

--- Runtime/UnityExtensions.cs
+++ Runtime/UnityExtensions.cs
@@ -6,20 +6,42 @@ namespace TeaGames.Unity.Utils.Runtime
     {
         public static T RequireComponent<T>(this GameObject gameObject)
         {
+#if DEBUG
             if (gameObject.TryGetComponent<T>(out var component))
                 return component;
             else
                 throw new MissingComponentException($"Missing {typeof(T).FullName} component of {gameObject.gameObject.name} game object!");
+#else
+            return gameObject.GetComponent<T>();
+#endif
         }
 
         public static T RequireComponentInChildren<T>(this GameObject gameObject)
         {
+#if DEBUG
             var component = gameObject.GetComponentInChildren<T>();
 
             if (component != null)
                 return component;
             else
                 throw new MissingComponentException($"Missing {typeof(T).FullName} component of {gameObject.gameObject.name} game object or it child!");
+#else
+            return gameObject.GetComponentInChildren<T>();
+#endif
+        }
+
+        public static T FindComponentInScene<T>(this GameObject gameObject) where T : Object
+        {
+#if DEBUG
+            var component = Object.FindObjectOfType<T>();
+
+            if (component != null)
+                return component;
+            else
+                throw new MissingComponentException($"Can't find component of type {typeof(T).FullName} in the scene!");
+#else
+            return Object.FindObjectOfType<T>();
+#endif
         }
     }
 }
