2f27974e7b4970d93a056faadeb22010f92e1ae5
sergei201613 <sgorey@bk.ru>
2022-09-04 16:09:35 +0800

    MonoExtensions.cs renamed to UnityExtensions.cs, Delay extension deleted

---
+++ Runtime/UnityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TeaGames.Unity.Utils.Runtime
+{
+    public static class UnityExtensions
+    {
+        public static T RequireComponent<T>(this GameObject gameObject)
+        {
+            if (gameObject.TryGetComponent<T>(out var component))
+                return component;
+            else
+                throw new MissingComponentException($"Missing {typeof(T).FullName} component of {gameObject.gameObject.name} game object!");
+        }
+
+        public static T RequireComponentInChildren<T>(this GameObject gameObject)
+        {
+            var component = gameObject.GetComponentInChildren<T>();
+
+            if (component != null)
+                return component;
+            else
+                throw new MissingComponentException($"Missing {typeof(T).FullName} component of {gameObject.gameObject.name} game object or it child!");
+        }
+    }
+}
