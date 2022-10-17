66f51ccb2d9d3cd9c9840926f264221f6d3d07bb
sergei201613 <sgorey@bk.ru>
2022-10-17 15:19:03 +0800

    Change Boot.cs

--- Runtime/Boot.cs
+++ Runtime/Boot.cs
@@ -4,12 +4,15 @@ namespace Sgorey.Unity.Utils.Runtime
 {
     public class Boot : MonoBehaviour
     {
+        public event System.Action BootComplete;
+
         [SerializeField] private string _startScene;
 
         private void Awake()
         {
             SceneHelper.AddSceneAsync(_startScene, () =>
             {
+                BootComplete?.Invoke();
                 Destroy(gameObject);
             });
         }
